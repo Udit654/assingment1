42ca0b859f04d1697a429db95803b2203634dc8c
Udit654 <42604590+Udit654@users.noreply.github.com>
2018-09-11 23:03:33 +0530

    assingments

---
+++ assingment5/static.java
@@ -0,0 +1,14 @@
+class function 
+{
+    static 
+	{
+        System.out.println("Static block is called automatically");
+    }
+}
+ 
+public class STATIC {
+    public static void main(String[] x) {
+ 
+       func f = new func();
+    }
+}
