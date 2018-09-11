8444fbf4b4df856d484145f629b4b32dc40fb8d0
Udit654 <42604590+Udit654@users.noreply.github.com>
2018-09-11 23:05:05 +0530

    assingment

---
+++ assingment6/Calling_Constructor.java
@@ -0,0 +1,20 @@
+class Base
+{
+	Base()
+	{
+		System.out.println("Default Contructor is called.");
+	}
+	
+	Base(int i){
+		this();
+		System.out.println("Parameterized constructor is called and value is " + i);
+	}	
+}
+
+public class Calling_Constructor
+{
+	public static void main(String[] x)
+	{
+		Base b = new Base(14);
+	}
+}
